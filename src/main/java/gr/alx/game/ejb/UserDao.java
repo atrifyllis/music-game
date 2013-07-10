7e7d3815dc7b43e8a5e7c581e55f7e7cff239fd6
Alexandros Trifyllis <trifyllisaa@dias.com.gr>
2013-07-10 16:30:14 +0300

    addded arquillian jta transaction so i can rollback test transactions added some more tests

--- src/main/java/gr/alx/game/ejb/UserDao.java
+++ src/main/java/gr/alx/game/ejb/UserDao.java
@@ -5,19 +5,29 @@ import gr.alx.game.model.GameUser;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 import java.util.List;
 
 @Stateless
-public class UserDao
-{
+public class UserDao {
     @PersistenceContext
     EntityManager em;
 
-    public List<GameUser> listUsers(){
+    public GameUser getUser(Long id) {
+        return em.find(GameUser.class, id);
+    }
+
+    public GameUser getUserByUsername(String username) {
+        Query query = em.createQuery("select u from GameUser u where u.username=:username");
+        query.setParameter("username", username);
+        return (GameUser) query.getSingleResult();
+    }
+
+    public List<GameUser> listUsers() {
         return em.createQuery("select u from GameUser u", GameUser.class).getResultList();
     }
 
-    public void addUser(GameUser user){
+    public void addUser(GameUser user) {
         em.persist(user);
     }
 }
--- src/test/java/gr/alx/game/ejb/UserDaoTest.java
+++ src/test/java/gr/alx/game/ejb/UserDaoTest.java
@@ -1,39 +1,55 @@
 package gr.alx.game.ejb;
 
-import gr.alx.game.ejb.UserDao;
-import javax.inject.Inject;
-
 import gr.alx.game.model.GameUser;
+import gr.alx.game.util.DatabaseUsersGenerator;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.arquillian.transaction.api.annotation.TransactionMode;
+import org.jboss.arquillian.transaction.api.annotation.Transactional;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
-import org.jboss.shrinkwrap.api.spec.JavaArchive;
 import org.jboss.shrinkwrap.api.asset.EmptyAsset;
-import org.junit.Assert;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import javax.inject.Inject;
+import java.util.List;
+
+import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
-import static org.hamcrest.core.Is.*;
 
 @RunWith(Arquillian.class)
-public class UserDaoTest
-{
-   @Inject
-   private UserDao userdao;
+public class UserDaoTest {
+    @Inject
+    private UserDao userdao;
 
-   @Deployment
-   public static JavaArchive createDeployment()
-   {
-      return ShrinkWrap.create(JavaArchive.class, "test.jar")
-            .addClass(UserDao.class)
-              .addPackage(GameUser.class.getPackage())
-            .addAsManifestResource("META-INF/persistence.xml", "persistence.xml")
-            .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
-   }
+    @Deployment
+    public static JavaArchive createDeployment() {
+        return ShrinkWrap.create(JavaArchive.class, "test.jar")
+                .addClasses(UserDao.class, DatabaseUsersGenerator.class)
+                .addPackage(GameUser.class.getPackage())
+                .addAsManifestResource("META-INF/persistence.xml", "persistence.xml")
+                .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
+    }
 
-   @Test
-   public void testIsDeployed()
-   {
-      Assert.assertNotNull(userdao);
-   }
+    @Test
+    public void testIsDeployed() {
+        assertNotNull(userdao);
+    }
+
+    @Test
+    public void testListUsers() {
+        List<GameUser> users = userdao.listUsers();
+        assertNotNull(users);
+        assertThat(users.size(), is(10));
+    }
+
+    @Test
+    @Transactional(TransactionMode.ROLLBACK)
+    public void testAddUser() {
+        userdao.addUser(new GameUser("test", "test@test.com"));
+        GameUser user = userdao.getUserByUsername("test");
+        assertNotNull(user);
+        assertThat(user.getEmail(), is("test@test.com"));
+    }
 }
