d41fded9b42063c12e81661084fce4e6c1c9cbb7
Alexandros Trifyllis <trifyllisaa@dias.com.gr>
2013-07-10 13:25:19 +0300

    added arquilian for integration testing. wrote first test class

---
+++ src/test/java/gr/alx/game/ejb/UserDaoTest.java
@@ -0,0 +1,39 @@
+package gr.alx.game.ejb;
+
+import gr.alx.game.ejb.UserDao;
+import javax.inject.Inject;
+
+import gr.alx.game.model.GameUser;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import static org.junit.Assert.*;
+import static org.hamcrest.core.Is.*;
+
+@RunWith(Arquillian.class)
+public class UserDaoTest
+{
+   @Inject
+   private UserDao userdao;
+
+   @Deployment
+   public static JavaArchive createDeployment()
+   {
+      return ShrinkWrap.create(JavaArchive.class, "test.jar")
+            .addClass(UserDao.class)
+              .addPackage(GameUser.class.getPackage())
+            .addAsManifestResource("META-INF/persistence.xml", "persistence.xml")
+            .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
+   }
+
+   @Test
+   public void testIsDeployed()
+   {
+      Assert.assertNotNull(userdao);
+   }
+}
